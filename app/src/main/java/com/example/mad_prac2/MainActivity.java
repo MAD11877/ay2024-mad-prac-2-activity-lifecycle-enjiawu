30b513d3ec76e9422aed557bb86062ccca4afda9
enjia <enjia.wu@gmail.com>
2024-04-21 22:20:34 +0800

    updated files 2

--- app/src/main/java/com/example/mad_prac2/MainActivity.java
+++ app/src/main/java/com/example/mad_prac2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mad_prac2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +23,17 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    //Initialize a new User object
+    User user = new User("John Doe", "MAD Developer", 1, false);
+
+    //Get the TextViews and Button from the layout
+    TextView tvName = findViewById(R.id.tvName);
+    TextView tvDescription = findViewById(R.id.tvDescription);
+    Button btnFollow = findViewById(R.id.btnFollow);
+
+    //Set the TextViews with the User's name, description and default button message
+    tvName.setText(user.name);
+    tvDescription.setText(user.description);
+    btnFollow.setText("Follow");
 }
